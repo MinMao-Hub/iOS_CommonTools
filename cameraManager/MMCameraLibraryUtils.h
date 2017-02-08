34d9c7c5851c7c7cb5b6a78142322ec9dccadfe7
gyh <guoyonghong@jfpal.com>
2017-02-08 14:21:02 +0800

    add cameraManager

---
+++ cameraManager/MMCameraLibraryUtils.h
@@ -0,0 +1,188 @@
+//
+//  MMCameraLibraryUtils.h
+//  DDWeexDemo
+//
+//  Created by 郭永红 on 2017/1/4.
+//  Copyright © 2017年 郭永红. All rights reserved.
+//
+
+
+/*****************************************************
+ *****************************************************
+ *
+ *   1. 打开相机拍照
+ *   2. 打开手机相册选取图片
+ *   3. 打开相机拍摄视频
+ *   4. 打开手机相册中选取视频
+ *   5. 保存图片到相册图库中
+ *   6. 保存视频到相册图库中
+ *
+ * * * 如果自定义覆盖页面的话，还需要以下功能需要去调用,具体用法见代码中的注释
+ *   7. 切换前后摄像头
+ *   8. 闪光灯打开与关闭
+ *
+ *****************************************************
+ */
+
+
+#import <Foundation/Foundation.h>
+
+@interface MMCameraLibraryUtils : NSObject
+
+@property (nonatomic, strong) UIView *photoOverFlowView;   //拍照时自定义的覆盖视图
+@property (nonatomic, strong) UIView *videoOverFlowView;    //拍视频时自定义的覆盖视图
+
+
+/**
+ 单例创建该对象
+
+ @return MMCameraLibraryUtils对象
+ */
++ (instancetype)sharedInstance;
+
+
+/**
+ 判断设备是否有摄像头 (现在的手机摄像头是必须有的，所以该方法基本无用)
+
+ @return 是否可用
+ */
+- (BOOL) cameraAvailable;
+
+
+/**
+ 前置摄像头是否可用
+
+ @return 是否可用
+ */
+- (BOOL) frontCameraAvailable;
+
+
+/**
+ 后置摄像头是否可用
+
+ @return 是否可用
+ */
+- (BOOL) rearCameraAvailable;
+
+
+
+/**
+ 弹出一个简单的ActionSheet来选择相册或者拍照来获取图片
+
+ @param completeImage 结束后的回调
+ *  *
+    *
+ 
+    #param image  照片的原图
+ 
+    #info信息里面包含如下数据，具体按照实际情况获取，若果未设置图片可编辑(`[self.imagePicker setAllowsEditing:YES]`),编辑后的图片为空,可能会导致奔溃
+    {
+        UIImage* original = [info objectForKey:UIImagePickerControllerOriginalImage];      //照片的原图
+     
+        UIImage* edit = [info objectForKey:UIImagePickerControllerEditedImage];            //裁剪后的图
+     
+        UIImage* crop = [info objectForKey:UIImagePickerControllerCropRect];               //图片裁剪后，剩下的图
+     
+        NSURL* url = [info objectForKey:UIImagePickerControllerMediaURL];                  //图片在相册中的url
+     
+        NSDictionary* metadata = [info objectForKey:UIImagePickerControllerMediaMetadata]; //获取图片的metadata数据信息
+    }
+ 
+ *  *
+    *
+ @param parentViewController 父控制器（需要弹出picker的控制器）
+ */
+- (void)showActionSheetPicker:(void (^)(UIImage *image, NSDictionary *info))completeImage onTarget:(UIViewController *)parentViewController;
+
+
+
+/**
+ 1. 调用相机拍摄照片
+
+ @param completeImage 拍照后的回调: 回调参数同上
+ @param isFront 是否使用前置摄像头
+ @param parentViewController 父控制器
+ */
+- (void)capturePhoto:(void (^)(UIImage *image, NSDictionary *info))completeImage isUseFrontDevice:(BOOL)isFront onTarget:(UIViewController *)parentViewController;
+
+
+
+/**
+ 2. 调用手机相册选取图片
+
+ @param completeImage 选取后的回调: 回调参数同上上
+ @param parentViewController 父控制器
+ */
+- (void)selectImageFromLibrary:(void (^)(UIImage *image, NSDictionary *info))completeImage onTarget:(UIViewController *)parentViewController;
+
+
+
+/**
+ 3. 调用相机拍摄视频
+
+ @param maximumDuration 拍摄最长时间 （如果不想限制时间，直接传 0 ）
+ @param isFront 是否使用前置摄像头
+ @param parentViewController 父控制器
+ @param completeVideo 选取完成回调: 参数为视频文件的绝对路径
+ */
+- (void)captureVideoMaximumDuration:(NSTimeInterval)maximumDuration isUseFrontDevice:(BOOL)isFront onTarget:(UIViewController *)parentViewController captureComplete:(void (^)(NSString *videoFilePath))completeVideo;
+
+
+
+/**
+ 4. 从手机相册中选取视频
+
+ @param completeVideo 选取完成回调: 参数为视频文件的绝对路径
+ @param parentViewController 父控制器
+ */
+- (void)selectVideoFromLibrary:(void (^)(NSString *videoFilePath))completeVideo onTarget:(UIViewController *)parentViewController;
+
+
+
+/**
+ 5. 保存图片到相册中
+
+ @param image image对象
+ */
+- (void)saveImageToAlbum:(UIImage *)image;
+
+
+
+/**
+ 6. 保存视频到相册中
+
+ @param videoFilePath 视频绝对路径
+ */
+- (void)saveVideoToAlbum:(NSString *)videoFilePath;
+
+
+
+/**
+ 7. 切换前后摄像头
+ */
+- (void)enchangeCameraDevice;
+
+
+
+/**
+ 8. 打开与关闭闪光灯
+
+ */
+- (void)flashModeOn;
+
+
+
+/**
+ 如果是自定义相机UI的话，还需要开放该方法来调用拍摄
+ */
+- (void)takePhoto;
+
+
+/**
+ 视频拍摄也一样，如果是自定义相机UI的话，还需要开放如下方法来调用拍摄开始与结束
+ */
+- (void)startVideo;
+
+- (void)stopVideo;
+
+@end
---
+++ cameraManager/MMCameraLibraryUtils.m
@@ -0,0 +1,342 @@
+//
+//  MMCameraLibraryUtils.m
+//  DDWeexDemo
+//
+//  Created by 郭永红 on 2017/1/4.
+//  Copyright © 2017年 郭永红. All rights reserved.
+//
+
+#import "MMCameraLibraryUtils.h"
+#import <AVFoundation/AVFoundation.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+
+@interface MMCameraLibraryUtils()<UIImagePickerControllerDelegate>
+
+@property (nonatomic, strong) UIImagePickerController *imagePicker;
+
+@property (nonatomic, copy) void (^imagePickerBlock)(UIImage *image, NSDictionary *info);
+@property (nonatomic, copy) void (^videoBlock)(NSString *videoFilePath);
+@property (nonatomic, strong) UIViewController *parentViewController;
+
+@end
+
+
+@implementation MMCameraLibraryUtils
+
++ (instancetype)sharedInstance {
+    static MMCameraLibraryUtils * shareInstance;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        shareInstance = [[MMCameraLibraryUtils alloc] init];
+        
+        shareInstance.imagePicker = [[UIImagePickerController alloc] init];
+        shareInstance.imagePicker.delegate = (id)shareInstance;
+        
+    });
+    return shareInstance;
+}
+
+
+// 判断设备是否有摄像头
+- (BOOL) cameraAvailable {
+    return [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera];
+}
+
+// 前面的摄像头是否可用
+- (BOOL) frontCameraAvailable {
+    return [UIImagePickerController isCameraDeviceAvailable:UIImagePickerControllerCameraDeviceFront];
+}
+
+// 后面的摄像头是否可用
+- (BOOL) rearCameraAvailable {
+    return [UIImagePickerController isCameraDeviceAvailable:UIImagePickerControllerCameraDeviceRear];
+}
+
+
+- (void)showActionSheetPicker:(void (^)(UIImage *image, NSDictionary *info))completeImage onTarget:(UIViewController *)parentViewController {
+    
+    self.parentViewController = parentViewController;
+    UIAlertController *actionSheet = [UIAlertController alertControllerWithTitle:@"选择照片来源" message:@"" preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    UIAlertAction *camera = [UIAlertAction actionWithTitle:@"拍照" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+        [self capturePhoto:^(UIImage *image, NSDictionary *info) {
+            completeImage(image, info);
+        } isUseFrontDevice:NO onTarget:parentViewController];
+    }];
+    
+    UIAlertAction *photoLibrary = [UIAlertAction actionWithTitle:@"手机相册" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [self selectImageFromLibrary:^(UIImage *image, NSDictionary *info) {
+            completeImage(image, info);
+        } onTarget:parentViewController];
+    }];
+    
+    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:NULL];
+    
+    if ([UIImagePickerController isSourceTypeAvailable:
+         UIImagePickerControllerSourceTypeCamera])
+    {
+        [actionSheet addAction:camera];
+        [actionSheet addAction:photoLibrary];
+        [actionSheet addAction:cancel];
+    }
+    else
+    {
+        [actionSheet addAction:photoLibrary];
+        [actionSheet addAction:cancel];
+    }
+    
+    [self.parentViewController presentViewController:actionSheet animated:YES completion:NULL];
+}
+
+
+- (void)capturePhoto:(void (^)(UIImage *image, NSDictionary *info))completeImage isUseFrontDevice:(BOOL)isFront onTarget:(UIViewController *)parentViewController {
+    
+    self.parentViewController = parentViewController;
+    
+    if (completeImage) {
+        self.imagePickerBlock = completeImage;
+    }
+    
+    self.imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    //是否允许编辑照片
+    [self.imagePicker setAllowsEditing:YES];
+    
+    //是否显示默认相机UI
+    [self.imagePicker setShowsCameraControls:_photoOverFlowView ? NO : YES];
+    [self.imagePicker setCameraDevice: (isFront && [self frontCameraAvailable]) ? UIImagePickerControllerCameraDeviceFront : UIImagePickerControllerCameraDeviceRear];
+    [self.imagePicker setCameraFlashMode:UIImagePickerControllerCameraFlashModeAuto];
+    [self.imagePicker setCameraOverlayView:_photoOverFlowView];
+    
+    //设置媒体类型
+    [self.imagePicker setMediaTypes:@[(NSString *)kUTTypeImage]];
+    
+    //设置拍摄模式 （拍摄照片或者视频）
+    [self.imagePicker setCameraCaptureMode:UIImagePickerControllerCameraCaptureModePhoto];
+    [self showImagePicker];
+}
+
+- (void)selectImageFromLibrary:(void (^)(UIImage *image, NSDictionary *info))completeImage onTarget:(UIViewController *)parentViewController {
+    
+    self.parentViewController = parentViewController;
+    
+    if (completeImage) {
+        self.imagePickerBlock = completeImage;
+    }
+    
+    self.imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    [self.imagePicker setAllowsEditing:YES];
+    
+    //媒体类型的话，就选常用的几种吧
+    [self.imagePicker setMediaTypes:@[(NSString *)kUTTypeImage,
+                                      (NSString *)kUTTypeJPEG,
+                                      (NSString *)kUTTypeGIF,
+                                      (NSString *)kUTTypePNG,
+                                      (NSString *)kUTTypeQuickTimeImage,
+                                      (NSString *)kUTTypeLivePhoto
+                                      ]];
+    
+    [self showImagePicker];
+    
+}
+
+- (void)captureVideoMaximumDuration:(NSTimeInterval)maximumDuration isUseFrontDevice:(BOOL)isFront onTarget:(UIViewController *)parentViewController captureComplete:(void (^)(NSString *videoFilePath))completeVideo {
+    
+    self.parentViewController = parentViewController;
+    
+    if (completeVideo) {
+        self.videoBlock = completeVideo;
+    }
+    
+    self.imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    [self.imagePicker setAllowsEditing:YES];
+    [self.imagePicker setShowsCameraControls:_videoOverFlowView ? NO : YES];
+    [self.imagePicker setCameraDevice: (isFront && [self frontCameraAvailable]) ? UIImagePickerControllerCameraDeviceFront : UIImagePickerControllerCameraDeviceRear];
+    [self.imagePicker setCameraFlashMode:UIImagePickerControllerCameraFlashModeAuto];
+    [self.imagePicker setCameraOverlayView:_videoOverFlowView];
+    // 设置录制视频的质量
+    [self.imagePicker setVideoQuality:UIImagePickerControllerQualityTypeHigh];
+    //设置最长摄像时间
+    [self.imagePicker setVideoMaximumDuration:maximumDuration < 0.1 ? MAXFLOAT : maximumDuration];
+    
+    [self.imagePicker setMediaTypes:@[(NSString *)kUTTypeMovie]];
+    
+    //设置拍摄模式 （拍摄视频）
+    [self.imagePicker setCameraCaptureMode:UIImagePickerControllerCameraCaptureModeVideo];
+    
+    [self showImagePicker];
+}
+
+- (void)selectVideoFromLibrary:(void (^)(NSString *videoFilePath))completeVideo onTarget:(UIViewController *)parentViewController {
+    self.parentViewController = parentViewController;
+    
+    if (completeVideo) {
+        self.videoBlock = completeVideo;
+    }
+    
+    self.imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    //媒体类型的话，就选常用的几种吧
+    [self.imagePicker setMediaTypes:@[(NSString *)kUTTypeMovie,
+                                      (NSString *)kUTTypeVideo,
+                                      (NSString *)kUTTypeQuickTimeMovie,
+                                      ]];
+    
+    [self showImagePicker];
+}
+
+
+- (void) showImagePicker {
+    
+    AVAuthorizationStatus authStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+    
+    switch (authStatus) {
+        case AVAuthorizationStatusNotDetermined:
+        {
+            [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted)
+             {
+                 if(granted)
+                 {
+                     [self.parentViewController presentViewController:self.imagePicker animated:YES completion:NULL];
+                 }
+                 else
+                 {
+                     [self showAlert:@"媒体(相机、相册)访问未授权"];
+                     return;
+                 }
+             }];
+        }
+            break;
+            
+        case AVAuthorizationStatusAuthorized:
+            [self.parentViewController presentViewController:self.imagePicker animated:YES completion:NULL];
+            break;
+            
+        default:
+            [self showAlert:@"请先在系统设置中对该应用开启相机、相册访问权限"];
+            break;
+    }
+}
+
+- (void)showAlert:(NSString *)message {
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"温馨提示" message:message preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleCancel handler:NULL];
+    [alert addAction:okAction];
+    
+    [self.parentViewController presentViewController:alert animated:YES completion:NULL];
+    
+}
+
+#pragma mark - UIImagePickerControllerDelegate
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info; {
+    
+    
+    if (picker.sourceType == UIImagePickerControllerSourceTypePhotoLibrary) {
+        if ([info valueForKey:UIImagePickerControllerOriginalImage]) {
+            UIImage *image = [info valueForKey:UIImagePickerControllerOriginalImage];
+            if (self.imagePickerBlock) {
+                self.imagePickerBlock(image, info);
+            }
+        }else{
+            if (self.videoBlock) {
+                self.videoBlock([info valueForKey:UIImagePickerControllerMediaURL]);
+            }
+        }
+    }else{
+        if (picker.cameraCaptureMode == UIImagePickerControllerCameraCaptureModeVideo) {
+            if (self.videoBlock) {
+                self.videoBlock([info objectForKey:UIImagePickerControllerMediaURL]);
+            }
+        }else{
+            UIImage *image = [info valueForKey:UIImagePickerControllerOriginalImage];
+            if (self.imagePickerBlock) {
+                self.imagePickerBlock(image, info);
+            }
+        }
+    }
+    
+    
+    NSLog(@"%@",info.description);
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    @try {
+        [picker dismissViewControllerAnimated:YES completion:NULL];
+    } @catch (NSException *exception) {
+        NSLog(@"%@",exception.description);
+    }
+}
+
+
+- (void)saveImageToAlbum:(UIImage *)image {
+    
+    UIImageWriteToSavedPhotosAlbum(image, self, @selector(imageSavedToPhotosAlbum:didFinishSavingWithError:contextInfo:), nil);
+}
+
+//保存照片成功后的回调
+- (void)imageSavedToPhotosAlbum:(UIImage *)image didFinishSavingWithError:(NSError *)error
+  contextInfo:(void *)contextInfo {
+    
+    if (!error) {
+        NSLog(@"保存图片到相册成功");
+    }else {
+        NSLog(@"保存图片到相册发生错误，错误信息%@",error);
+    }
+}
+
+- (void)saveVideoToAlbum:(NSString *)videoFilePath {
+    
+    UISaveVideoAtPathToSavedPhotosAlbum(videoFilePath, self, @selector(videoSavedToPhotosAlbum:didFinishSavingWithError:contextInfo:), nil);
+}
+
+- (void)videoSavedToPhotosAlbum:(NSString *)videoPath didFinishSavingWithError:(NSError *)error
+  contextInfo:(void *)contextInfo{
+    if (!error) {
+        NSLog(@"保存视频到相册成功");
+    }else {
+        NSLog(@"保存视频到相册发生错误，错误信息%@",error);
+    }
+}
+
+- (void)enchangeCameraDevice {
+    
+    if (self.imagePicker.cameraDevice == UIImagePickerControllerCameraDeviceRear) {
+        self.imagePicker.cameraDevice = UIImagePickerControllerCameraDeviceFront;
+    }
+    else
+    {
+        self.imagePicker.cameraDevice = UIImagePickerControllerCameraDeviceRear;
+    }
+}
+
+
+- (void)flashModeOn {
+    
+    if (self.imagePicker.cameraFlashMode == UIImagePickerControllerCameraFlashModeAuto || self.imagePicker.cameraFlashMode == UIImagePickerControllerCameraFlashModeOff) {
+        self.imagePicker.cameraFlashMode = UIImagePickerControllerCameraFlashModeOn;
+    }
+    else
+    {
+        self.imagePicker.cameraFlashMode = UIImagePickerControllerCameraFlashModeOff;
+    }
+}
+
+- (void)takePhoto {
+    [self.imagePicker takePicture];
+}
+
+- (void)startVideo {
+    [self.imagePicker startVideoCapture];
+}
+
+- (void)stopVideo {
+    [self.imagePicker stopVideoCapture];
+}
+
+@end
